368dbdda0194c963f5f8c4a8708af6ad4115ba76
chen1ting <80753389+chen1ting@users.noreply.github.com>
2022-05-04 00:29:26 +0800

    sorting algos
    Heap Sort Needs Fixing

--- Sorting/SortingAlgos.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by 陈一婷 on 3/5/22.
-//
-
-#include "SortingAlgos.h"
--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
-#include "SortingAlgos.h"
+#include "SortingAlgo.h"
 
 int main() {
-
+    int opt;
+    int n;
+    while(true){
+        cout<<"Enter List Size (enter negative integer to exit):\n";
+        cin>>n;
+        if(n<1){
+            cout<<"Negative Integer detected, program stopping...";
+            return 0;
+        }
+        SortingAlgo list(n);
+        list.optChoice();
+    }
 }
