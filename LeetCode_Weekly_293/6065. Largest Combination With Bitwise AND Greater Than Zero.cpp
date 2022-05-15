6b0348b4e41f330b864c12a42cc02fa27513d55e
chen1ting <80753389+chen1ting@users.noreply.github.com>
2022-05-15 16:20:48 +0800

    Accepted Questions

--- LeetCode_Weekly_293/6065. Largest Combination With Bitwise AND Greater Than Zero.cpp
+++ LeetCode_Weekly_293/6065. Largest Combination With Bitwise AND Greater Than Zero.cpp
@@ -8,5 +8,25 @@
  Each number in candidates may only be used once in each combination.
 
  Return the size of the largest combination of candidates with a bitwise AND greater than 0.
-
  */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int largestCombination(vector<int>& candidates) {
+//since input is smaller than 10^7, we can form a bitmap to store the bit position of each number
+    vector<int> bin_map(24, 0); //bin_map is short for binary map
+    for(int num : candidates){
+        int i = 0;
+        while (num != 0) {
+            bin_map[i++] += num % 2;
+            num = num / 2;
+        }
+    }
+    return *max_element(bin_map.begin(), bin_map.end());
+}
+
+int main(){
+    vector<int> candidates = {16,17,71,62,12,24,14};
+    cout << largestCombination(candidates);
+}
